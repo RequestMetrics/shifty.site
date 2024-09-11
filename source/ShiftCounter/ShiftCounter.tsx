6a7916fe86ec3c5f53b4ae8be26ee5790d1e152f
Todd Gardner <todd@g12solutions.com>
2024-09-11 10:02:15 -0500

    Simplify the CLS calculation

--- source/ShiftCounter/ShiftCounter.tsx
+++ source/ShiftCounter/ShiftCounter.tsx
@@ -5,13 +5,22 @@ import { getRandomInteger } from "../util/getRandomInteger";
 import { isSafari } from "../util/isSafari";
 import "./ShiftCounter.scss";
 
+/**
+ * The LayoutShift interface of the Performance API provides insights into the layout stability of web pages based on movements of the elements on the page.
+ *
+ * [MDN Reference](https://developer.mozilla.org/en-US/docs/Web/API/LayoutShift)
+ */
+interface CustomLayoutShift extends PerformanceEntry {
+  readonly value: number;
+}
+
 interface ShiftCounterState {
   cls: number
 }
 
 export class ShiftCounter extends Component<any, ShiftCounterState> {
 
-  private observer : PerformanceObserver;
+  private observer: PerformanceObserver;
 
   constructor() {
     super();
@@ -21,7 +30,9 @@ export class ShiftCounter extends Component<any, ShiftCounterState> {
   }
 
   componentDidMount(): void {
+
     if (isSafari()) {
+      console.info("approximating layout shift value on Safari");
       GameTimer.onTick((tick) => {
         let cls = this.state.cls;
         cls = cls + ((getRandomInteger(5000, 10000) + (tick)) / 10000);
@@ -30,11 +41,14 @@ export class ShiftCounter extends Component<any, ShiftCounterState> {
       })
     }
     else {
-      this.observer = new PerformanceObserver(entryList => {
-        let entries = entryList.getEntries() || [];
-        let cls = entries.reduce((cls, entry: any, index) => {
-          return cls + entry.value;
-        }, this.state.cls);
+      let cls = this.state.cls;
+      this.observer = new PerformanceObserver((list: PerformanceObserverEntryList) => {
+        Promise.resolve().then(() => {
+          list.getEntries().forEach((entry: CustomLayoutShift) => {
+            cls += entry.value
+          });
+          this.setState({ cls });
+        })
         this.setState({ cls });
         GameController.cls = cls;
       });
@@ -46,12 +60,12 @@ export class ShiftCounter extends Component<any, ShiftCounterState> {
     this.observer.disconnect();
   }
 
-  render() : h.JSX.Element {
+  render(): h.JSX.Element {
     return (
       <div class="shift-counter">
         <div class="label">CLS</div>
         <div class="value">
-          { this.state.cls.toFixed(4) }
+          {this.state.cls.toFixed(4)}
         </div>
       </div>
     )
